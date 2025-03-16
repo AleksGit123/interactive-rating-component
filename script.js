f5f05d24f94ad2bac34f9aa96b85e11cf77b4e97
AleksGit123 <aleksichokheli03@gmail.com>
2025-03-16 10:35:23 +0400

    html+css+hovers

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let num1 = document.querySelector(".num1");
+let num2 = document.querySelector(".num2");
+let num3 = document.querySelector(".num3");
+let num4 = document.querySelector(".num4");
+let num5 = document.querySelector(".num5");
+
+
+num1.addEventListener("mouseover",() =>{
+    
+})
